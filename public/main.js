f5a27852e7980baaf5f84e1f89889a9d0d6d9089
rodolfodom <rodolfodominguez60@aragon.unam.mx>
2021-04-21 08:36:42 -0500

    feat/sendUserData: I finished functon to send user data to newsletter list

--- public/main.js
+++ public/main.js
@@ -1,10 +1,23 @@
 import promosSlider from "./assets/scripts/promotionalSlider.js"
 import productsSlider from "./assets/scripts/productsSlider.js"
 import productSlides from "./assets/scripts/productSlideGenerator.js"
+import sendData from "./assets/scripts/newsLetter.js";
 
+const d = document,
+newsletterForm = d.getElementById("newsletter-form"); 
 
-document.addEventListener("DOMContentLoaded", async ()=>{
+d.addEventListener("DOMContentLoaded", async ()=>{
     await productSlides()
     promosSlider();
     productsSlider();
-})
+});
+
+newsletterForm.addEventListener("submit", async (e)=>{
+    e.preventDefault();
+    const data = {
+        name : e.target.name.value,
+        email: e.target.email.value,
+    }
+
+    await sendData(data);
+});
