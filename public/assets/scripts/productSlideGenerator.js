1588e8b8ff29b4533588e80d6f61d775deb46928
rodolfodom <rodolfodominguez60@aragon.unam.mx>
2021-04-21 07:51:41 -0500

    feat/productCard: I fineshed the product card generator function

---
+++ public/assets/scripts/productSlideGenerator.js
@@ -0,0 +1,25 @@
+import getProducts from "./getProducts.js";
+
+const d = document;
+
+export default async function geberateSlide() {
+    const template = d.getElementById("product-card").content,
+    wrapper = d.querySelector("#products-container .swiper-wrapper"),
+    fragment = d.createDocumentFragment(),
+    products = await getProducts();
+    products.forEach(product => {
+        template.querySelector(".product-img img").src = product.imageUrl;
+        template.querySelector(".product-img img").alt = product.productName;
+        template.getElementById("productName").textContent = product.productName;
+        template.querySelector(".score").innerHTML = '<i class="fas fa-star"></i>'.repeat(product.stars) + '<i class="far fa-star"></i>'.repeat(5 - product.stars);
+        if(product.listPrice) template.querySelector(".last-price").textContent = `de $${product.listPrice}`;
+        template.querySelector(".actual-price").textContent = `por $${product.price}`;
+        template.querySelector(".installments").textContent = (product.installments.length > 0)?`ou em ${product.installments[0].quantity}x de R$ ${product.installments[0].value}` : "";
+        template.querySelector("button").dataset.productId = product.productId;
+        const clone = d.importNode(template, true);
+        fragment.appendChild(clone);
+    });
+
+    wrapper.appendChild(fragment);
+    
+}
--- public/main.js
+++ public/main.js
@@ -1,5 +1,10 @@
 import promosSlider from "./assets/scripts/promotionalSlider.js"
 import productsSlider from "./assets/scripts/productsSlider.js"
+import productSlides from "./assets/scripts/productSlideGenerator.js"
 
-promosSlider();
-productsSlider();
+
+document.addEventListener("DOMContentLoaded", async ()=>{
+    await productSlides()
+    promosSlider();
+    productsSlider();
+})
