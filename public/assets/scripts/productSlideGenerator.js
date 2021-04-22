7433fe0f84e469e3c5e54494169cc2f86c272a7c
rodolfodom <rodolfodominguez60@aragon.unam.mx>
2021-04-21 22:37:14 -0500

    fix/price Format: I fix the price format

--- public/assets/scripts/productSlideGenerator.js
+++ public/assets/scripts/productSlideGenerator.js
@@ -8,12 +8,13 @@ export default async function geberateSlide() {
     fragment = d.createDocumentFragment(),
     products = await getProducts();
     products.forEach(product => {
+        const formatedPrice = product.price.toString().slice(0, -2) + "." + product.price.toString().slice(-2);
         template.querySelector(".product-img img").src = product.imageUrl;
         template.querySelector(".product-img img").alt = product.productName;
         template.getElementById("productName").textContent = product.productName;
         template.querySelector(".score").innerHTML = '<i class="fas fa-star"></i>'.repeat(product.stars) + '<i class="far fa-star"></i>'.repeat(5 - product.stars);
-        if(product.listPrice) template.querySelector(".last-price").textContent = `de $${product.listPrice}`;
-        template.querySelector(".actual-price").textContent = `por $${product.price}`;
+        if(product.listPrice) template.querySelector(".last-price").textContent = `de $${product.listPrice.toString().slice(0, -2) + "." + product.listPrice.toString().slice(-2)}`;
+        template.querySelector(".actual-price").textContent = `por $${formatedPrice}`;
         template.querySelector(".installments").textContent = (product.installments.length > 0)?`ou em ${product.installments[0].quantity}x de R$ ${product.installments[0].value}` : "";
         template.querySelector("button").dataset.productId = product.productId;
         const clone = d.importNode(template, true);
